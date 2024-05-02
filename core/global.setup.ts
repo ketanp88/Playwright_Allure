b99e00db32e43b1f7c9eb8c1269711d042064c82
Ketan Pardeshi <ketan_pardeshiarun@yahoo.com>
2024-05-02 21:32:54 +1000

    Added changes

--- core/global.setup.ts
+++ core/global.setup.ts
@@ -1,11 +1,32 @@
 import type { FullConfig } from '@playwright/test';
 import { unlink } from 'node:fs/promises';
+import fs from 'fs';
 
 async function globalSetup(config: FullConfig) {
     try {
-        await unlink('ExecutionLogs.log');
+        await deleteFolderRecursive('./Logs');
+        await deleteFolderRecursive('./allure-results');
+        await deleteFolderRecursive('./allure-report');
       } catch (error) {
       }
+}
+
+
+async function deleteFolderRecursive(path: string) {
+  if (fs.existsSync(path)) {
+      fs.readdirSync(path).forEach((file) => {
+          const curPath = `${path}/${file}`;
+          if (fs.lstatSync(curPath).isDirectory()) {
+              // Recursively delete subfolders
+              deleteFolderRecursive(curPath);
+          } else {
+              // Delete files
+              fs.unlinkSync(curPath);
+          }
+      });
+      // Delete the empty folder once all files and subfolders are deleted
+      fs.rmdirSync(path);
+  }
 }
 
 export default globalSetup;
